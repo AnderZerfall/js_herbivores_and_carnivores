2cae36ec38f62ce655b560f412ea2fd649eb7b97
Fukuro <anya.androschuk@gmail.com>
2024-11-19 12:47:41 +0200

    Task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,33 +1,36 @@
 'use strict';
 
 class Animal {
-  constructor(health = 100, name) {
-    this.health = health;
+  static alive = [];
+
+  constructor(name, health = 100) {
     this.name = name;
+    this.health = health;
+
+    if (this.health > 0) {
+      Animal.alive.push(this);
+    }
+  }
+
+  checkHealth() {
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+    }
   }
 }
 
 class Herbivore extends Animal {
-  constructor(health, name) {
-    super(health, name)
-    this.hidden = false;
-  }
-
+  hidden = false;
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(health, name) {
-    super(health, name)
-  }
-
   bite(target) {
-
-    if (target instanceof Herbivore
-      && target.hidden === false) {
-      herbivore.health -= 50;
+    if (target instanceof Herbivore && target.hidden === false) {
+      target.health -= 50;
+      target.checkHealth();
     }
   }
 }
